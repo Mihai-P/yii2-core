82bb0316d2393f2d05d624718406cbc8bbd6c3b0
2ez.mihaipetrescu_2ez <mihai.petrescu@webmanage.com>
2014-08-30 15:46:13 +1000

    adding some helpers

---
+++ components/Helpers.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace core\components;
+
+/**
+ * ErrorAction displays application errors using a specified view.
+ *
+ * To use ErrorAction, you need to do the following steps:
+ *
+ * First, declare an action of ErrorAction type in the `actions()` method of your `SiteController`
+ * class (or whatever controller you prefer), like the following:
+ *
+ * ```php
+ * public function actions()
+ * {
+ *     return [
+ *         'error' => ['class' => 'yii\web\ErrorAction'],
+ *     ];
+ * }
+ * ```
+ *
+ * Then, create a view file for this action. If the route of your error action is `site/error`, then
+ * the view file should be `views/site/error.php`. In this view file, the following variables are available:
+ *
+ * - `$name`: the error name
+ * - `$message`: the error message
+ * - `$exception`: the exception being handled
+ *
+ * Finally, configure the "errorHandler" application component as follows,
+ *
+ * ```php
+ * 'errorHandler' => [
+ *     'errorAction' => 'site/error',
+ * ]
+ * ```
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Helpers
+{
+	/**
+	 * Protects the email from bots
+	 * I have copied and modified this http://www.maurits.vdschee.nl/php_hide_email/
+	 *
+	 * @param string $email the email address
+	 * @return string 
+	 */	
+	
+    public static function hideEmail($email) { 
+    	$character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+		$key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999);
+  		for ($i=0;$i<strlen($email);$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])];
+  		
+  		$script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";';
+		$script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
+		$script.= 'document.getElementById("'.$id.'").innerHTML="<a href=\\"mailto:"+d+"\\">"+d+"</a>"';
+		$script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; 
+		$script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>';
+
+		return '<span id="'.$id.'">[javascript protected email address]</span>'.$script;
+	}
+}
