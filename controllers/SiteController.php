bbe2369826910aa21393d865909ed67cfb1feadb
2ez.mihaipetrescu_2ez <mihai.petrescu@webmanage.com>
2014-09-18 16:47:21 +1000

    site controller as the default controller

---
+++ controllers/SiteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace core\controllers;
+
+use Yii;
+use yii\web\BadRequestHttpException;
+use yii\web\HttpException;
+use yii\base\Security;
+use core\models\LoginForm;
+use core\models\Administrator;
+use core\models\Email;
+use core\components\Controller;
+
+class SiteController extends Controller
+{
+	/**
+	 * @var \core\Module
+	 */
+	public $module;
+
+	private $loginAttemptsVar = '__LoginAttemptsCount';
+
+	public function behaviors()
+	{
+		return [
+			'access' => [
+				'class' => \yii\filters\AccessControl::className(),
+				'only' => ['index'],
+				'rules' => [
+					[
+						'actions' => ['index'],
+						'allow' => true,
+						'roles' => ['@'],
+					],
+				],
+			],
+		];
+	}
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+	public function actionIndex()
+	{
+		$this->layout = static::FORM_LAYOUT;
+		return $this->render('index', []);
+	}
+}
