3d69a720ce310b38c3ef2ea1d4fb13bc7cb5495e
2ez.mihaipetrescu_2ez <mihai.petrescu@webmanage.com>
2014-10-12 11:39:53 +1100

    test changes

--- views/page/_test.php
+++ views/page/_test.php
@@ -1,3 +1,5 @@
 <?php
 	echo $form->field($model, 'content')->textarea(['class' => 'editor']);
 	echo $form->field($model->object('SuccessMessage'), 'content')->textInput(['name' => 'Object[SuccessMessage]', 'id' => 'Object-SuccessMessage']);
+    //echo $form->field($model, 'content')->widget(theme\widgets\ImageButton\ImageButtonWidget::className());
+    //echo $form->field($model->object('Button'), 'content')->widget(theme\widgets\ImageButton\ImageButtonWidget::className(), ['options' => ['id' => 'object-button', 'name' => 'Object[Button]']])->hint('Hello World !!!');
