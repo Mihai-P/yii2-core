d01094af63d2d7085b3914be510ae6f35fb145ca
Mihai Petrescu <mihai.petrescu@gmail.com>
2014-11-10 20:37:26 +1100

    RedirectWidget fix

--- widgets/RedirectWidget.php
+++ widgets/RedirectWidget.php
@@ -45,8 +45,7 @@ class RedirectWidget extends Widget
 	 */
 	public function run()
 	{
-        die($this->url);
-		echo $this->render($this->view,
+		return $this->render($this->view,
 			ArrayHelper::merge(array(
 				'id' => $this->getId(),
 				'url' => $this->url,
