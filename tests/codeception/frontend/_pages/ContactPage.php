ec1f4128b72780b59a518c6a0dd874931a93bce1
Mihai Petrescu <mihai.petrescu@gmail.com>
2014-11-13 08:38:07 +1100

    working on the tests

--- tests/codeception/frontend/_pages/ContactPage.php
+++ tests/codeception/frontend/_pages/ContactPage.php
@@ -10,7 +10,7 @@ use yii\codeception\BasePage;
  */
 class ContactPage extends BasePage
 {
-    public $route = 'site/contact';
+    public $route = '/contact';
 
     /**
      * @param array $contactData
