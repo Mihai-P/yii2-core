bbc0d81c771120a0b1de06f5c160e44c7a873e63
Mihai Petrescu <mihai.petrescu@gmail.com>
2015-05-09 18:08:14 +1000

    editor without stripping classes

--- views/page/_simple.php
+++ views/page/_simple.php
@@ -5,5 +5,5 @@ use mihaildev\elfinder\ElFinder;
 <?= $form->field($model, 'h1')->textInput(['maxlength' => 255]) ?>
 
 <?= $form->field($model, 'content')->widget(CKEditor::className(),[
-    'editorOptions' => ElFinder::ckeditorOptions('elfinder', ['preset' => 'full', 'inline' => false]),
+    'editorOptions' => ElFinder::ckeditorOptions('elfinder', ['preset' => 'full', 'inline' => false] + ["allowedContent" => true, "extraAllowedContent" => '*{*}']),
 ]) ?>
